b3b9325850a985fe96d64949a2b2d138fc2ba071
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-09-11 22:03:31 +0800

    updated time complexity

--- Arrays/2707/medium_extra_characters_in_a_string.py
+++ Arrays/2707/medium_extra_characters_in_a_string.py
@@ -1,3 +1,13 @@
+"""
+Time Complexity: 
+For loop -> O(n)
+sentence.find(word) -> O(n) to find the word
+sentence.replace -> O(n)
+Each iteration of loop, you're doing O(n + n), which gives rise to O(2n)
+
+Total: O(n * 2n) = O( 2 n^2) = O(n ^ 2) (not v good)
+"""
+
 class Solution(object):
     def minExtraChar(self, s, dictionary):
         """
@@ -13,6 +23,7 @@ class Solution(object):
         sentence = s
         for word in dictionary:
             word_start_index = sentence.find(word)
+            # the word exists in the sentence
             if word_start_index != -1:
                 sentence = sentence.replace(word, "")
 
