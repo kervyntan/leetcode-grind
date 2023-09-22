1b9330210043432bf6d4b27bee6a1bde508eb99b
Kervyn <kervyntan@gmail.com>
2023-09-22 12:46:50 +0800

    updated

---
+++ interview.js
@@ -0,0 +1,18 @@
+// Given [A, B, C], create a function that returns "A says hi to B and C. B says hi to A and C. C says hi to A and B"
+const greetings = (array) => {
+    // String concatenation
+    // += string concatenation is most efficient in JS (?)
+    // can consider including a function that prints out "says hi to"
+
+    // hardcode or use a loop?
+    let res = ''
+    // return array[0] += ' says hi to ' + array[1] +
+}
+
+const sayHi = (greeter, receipient1, receipient2) => {
+
+}
+
+greetings(['A', 'B' ,'C'])
+
+// GIven 2 strings of lowercase a-z characters, return an array of characters that appears at least n times in both strings
