6ec74f5268a0ecd6295c3199a1b0cd03cb98cc8b
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-09-30 00:00:21 +0800

    min heap

--- Trees/heaps.py
+++ Trees/heaps.py
@@ -2,5 +2,5 @@
 # In a heap, invariance when adding nodes is must maintain a complete binary tree
 # always append nodes to the left and right child of the 2nd last level, to fill up the last level first (bfs)
 
-# Order property
+# Order property (MIN heap)
 # For each node, the values in the left and right subtree must be greater than the root value
