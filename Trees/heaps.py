ffdbd4b47c91f5fe1de713c51c942a145b9a1c4f
Kervyn <73232587+kervyntan@users.noreply.github.com>
2023-09-29 23:59:51 +0800

    added structure and order properties for heaps

---
+++ Trees/heaps.py
@@ -0,0 +1,6 @@
+# Complete binary tree -> all levels except the last level has NO missing nodes
+# In a heap, invariance when adding nodes is must maintain a complete binary tree
+# always append nodes to the left and right child of the 2nd last level, to fill up the last level first (bfs)
+
+# Order property
+# For each node, the values in the left and right subtree must be greater than the root value
