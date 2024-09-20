e6c31bd51d92034bb14b5212922044ca055b67e3
Kervyn <73232587+kervyntan@users.noreply.github.com>
2024-09-21 00:26:27 +0800

    Update hash_table_implementation.py

--- Hashing/hash_table_implementation.py
+++ Hashing/hash_table_implementation.py
@@ -12,6 +12,7 @@ class HashTable:
         self.slots = [None for i in range(self.size)]
         # How many ACTUAL items are currently in the hashtable
         self.count = 0
+        self.MAXLOADFACTOR = 0.65 # n / k, n = number of used slots & k = total number of slots
     
     def _hash(self, key):
         mult = 1
@@ -27,3 +28,39 @@ class HashTable:
         # To ensure our hash value remains within the range 0 - self.size (number of slots in our hashtable)
         # Since remainder is never larger than it's divisor
         return hash_value % self.size
+    
+    def growth(self):
+        new_hash_table = HashTable()
+        new_hash_table.size = 2 * self.size
+        new_hash_table.slots = [None for i in range(new_hash_table.size)]
+        
+        for i in range(self.size):
+            # If existing hashtable has value at that index
+            # Populate it into the new hashtable
+            if self.slots[i] != None:
+                new_hash_table.put(self.slots[i].key, self.slots[i].value)
+        
+        self.size = new_hash_table.size
+        self.slots = new_hash_table.slots
+    
+    def check_growth(self):
+        loadfactor = self.count / self.size
+        if loadfactor > self.MAXLOADFACTOR:
+            self.growth()
+    
+    # Inserting elements into the hashtable
+    def put(self, key, value):
+        item = HashItem(key, value)
+        hash = self._hash(key)
+        while self.slots[hash] != None:
+            if self.slots[hash].key == key:
+                # If the key already exists
+                break
+            
+            hash = (hash + 1) % self.size # Linear probing 
+        if self.slots[hash] == None:
+            # Add to the count since new item added 
+            self.count += 1
+        self.slots[hash] = item
+        self.check_growth()
+            
